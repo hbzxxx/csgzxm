350c9faa2de58306ccd5e61e7ff0a3cb4ac54075
ㅤ <monkeycode-ai@chaitin.com>
2026-03-10 09:58:10 +0000

    refactor: 精简ArchiveGenerator只保留地图解锁和玩家等级功能 Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

--- Assets/ArchiveGenerator.cs
+++ Assets/ArchiveGenerator.cs
@@ -4,73 +4,36 @@ using Framework.Data;
 using cfg;
 
 /// <summary>
-/// 存档生成器 - 修改玩家等级和地图解锁状态
+/// 存档生成器 - 解锁所有地图和关卡，设置玩家等级
 /// </summary>
 public class ArchiveGenerator : MonoBehaviour
 {
-    private static ArchiveGenerator _instance;
-    public static ArchiveGenerator Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<ArchiveGenerator>();
-                if (_instance == null)
-                {
-                    GameObject singletonObj = new GameObject("ArchiveGenerator_Singleton");
-                    _instance = singletonObj.AddComponent<ArchiveGenerator>();
-                    DontDestroyOnLoad(singletonObj);
-                }
-            }
-            return _instance;
-        }
-    }
-
+    public static ArchiveGenerator Instance { get; private set; }
     public GameInfo _gameInfo;
 
-    [Header("玩家等级设置")]
+    [Header("玩家等级")]
     public int playerLevel = 9999;
 
     [ContextMenu("生成存档")]
-    public void GenerateFullUnlockArchive()
+    public void GenerateArchive()
     {
-        GameInfo gameInfo = CreateModifiedGameInfo();
-        _gameInfo = gameInfo;
+        _gameInfo = ModifyGameInfo(RoleManager.Instance._CurGameInfo);
         Debug.Log($"存档已生成，玩家等级: {playerLevel}");
     }
 
-    public GameInfo GetGameInfo()
+    private GameInfo ModifyGameInfo(GameInfo gameInfo)
     {
-        return _gameInfo;
-    }
-
-    private GameInfo CreateModifiedGameInfo()
-    {
-        GameInfo gameInfo = RoleManager.Instance._CurGameInfo;
-
-        gameInfo.playerPeople = CreateMaxLevelPlayer(gameInfo.playerPeople);
-        gameInfo.AllMapData = CreateAllUnlockedMaps();
-
+        if (gameInfo.playerPeople != null)
+        {
+            gameInfo.playerPeople.studentLevel = playerLevel;
+        }
+        gameInfo.AllMapData = CreateUnlockedMaps();
         return gameInfo;
     }
 
-    private PeopleData CreateMaxLevelPlayer(PeopleData originalPlayer)
+    private AllMapData CreateUnlockedMaps()
     {
-        if (originalPlayer != null)
-        {
-            originalPlayer.studentLevel = playerLevel;
-            if (originalPlayer.curEquipItemList == null)
-            {
-                originalPlayer.curEquipItemList = new List<ItemData> { null, null, null, null, null, null };
-            }
-        }
-        return originalPlayer;
-    }
-
-    private AllMapData CreateAllUnlockedMaps()
-    {
-        AllMapData mapData = new AllMapData
+        var mapData = new AllMapData
         {
             CurChoosedMapId = 1,
             CurChoosedXianMenMapId = 1,
@@ -78,14 +41,11 @@ public class ArchiveGenerator : MonoBehaviour
         };
 
         if (DataTable._mapList == null || DataTable._mapList.Count == 0)
-        {
             return mapData;
-        }
 
-        for (int i = 0; i < DataTable._mapList.Count; i++)
+        foreach (var setting in DataTable._mapList)
         {
-            MapSetting setting = DataTable._mapList[i];
-            SingleMapData map = new SingleMapData
+            var map = new SingleMapData
             {
                 MapId = setting.Id.ToInt32(),
                 MapStatus = 2,
@@ -95,38 +55,28 @@ public class ArchiveGenerator : MonoBehaviour
                 CurAwardList = new List<ItemData>()
             };
 
-            List<string> levelIdList = CommonUtil.SplitCfgStringOneDepth(setting.LevelIdList);
-            for (int j = 0; j < levelIdList.Count; j++)
+            foreach (var levelId in CommonUtil.SplitCfgStringOneDepth(setting.LevelIdList))
             {
-                string levelIdStr = levelIdList[j];
-                if (string.IsNullOrEmpty(levelIdStr) || levelIdStr == "0")
-                    continue;
-
-                SingleLevelData level = new SingleLevelData
+                if (string.IsNullOrEmpty(levelId) || levelId == "0") continue;
+                map.LevelList.Add(new SingleLevelData
                 {
-                    LevelId = levelIdStr,
+                    LevelId = levelId,
                     LevelStatus = 2,
                     HaveAccomplished = true,
                     Enemy = new List<PeopleData>()
-                };
-                map.LevelList.Add(level);
+                });
             }
 
-            List<string> fixedLevelIdList = CommonUtil.SplitCfgStringOneDepth(setting.FixedLevelIdList);
-            for (int j = 0; j < fixedLevelIdList.Count; j++)
+            foreach (var levelId in CommonUtil.SplitCfgStringOneDepth(setting.FixedLevelIdList))
             {
-                string levelIdStr = fixedLevelIdList[j];
-                if (string.IsNullOrEmpty(levelIdStr) || levelIdStr == "0")
-                    continue;
-
-                SingleLevelData level = new SingleLevelData
+                if (string.IsNullOrEmpty(levelId) || levelId == "0") continue;
+                map.FixedLevelList.Add(new SingleLevelData
                 {
-                    LevelId = levelIdStr,
+                    LevelId = levelId,
                     LevelStatus = 2,
                     HaveAccomplished = true,
                     Enemy = new List<PeopleData>()
-                };
-                map.FixedLevelList.Add(level);
+                });
             }
 
             mapData.MapList.Add(map);
